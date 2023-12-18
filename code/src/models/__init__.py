269a3f38fa380c12f075e289c576ad2e0f11c751
KIM EUNWOO <kimeunwoo0125@gmail.com>
2023-12-18 04:23:18 +0000

    add FM_MLP_paralles model

--- code/src/models/__init__.py
+++ code/src/models/__init__.py
@@ -5,3 +5,4 @@ from .WDN.WDN_model import WideAndDeepModel
 from .DCN.DCN_model import DeepCrossNetworkModel
 from .CNN_FM.CNN_FM_model import CNN_FM
 from .DeepCoNN.DeepCoNN_model import DeepCoNN
+from .FM_MLP_parallel.FM_MLP_parallel import FM_MLP_parallel
